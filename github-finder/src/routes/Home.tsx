d412e8ba8dc0f3c2224b9bd143f2c5eb6ef185e6
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-04-22 21:37:35 -0300

    Update Styles, loading and details

--- github-finder/src/routes/Home.tsx
+++ github-finder/src/routes/Home.tsx
@@ -3,14 +3,30 @@ import { UserProps } from "../types/user";
 import Search from "../components/Search";
 
 import { useState } from "react";
+import User from "../components/User";
+import Error from "../components/Error";
+import Loader from "../components/Loader";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
+  const [isLoding, setIsLoding] = useState(false);
 
   const loadUser = async (userName: string) => {
+    setIsLoding(true);
+    setError(false);
+    setUser(null);
+
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    setIsLoding(false);
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
 
     const { avatar_url, login, location, followers, following } = data;
 
@@ -27,7 +43,9 @@ const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser} />
-      {user && <p>{user.login}</p>}
+      {isLoding && <Loader />}
+      {user && <User {...user} />}
+      {error && <Error />}
     </div>
   );
 };
