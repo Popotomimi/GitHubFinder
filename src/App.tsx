4866f59fa5167069e172190620d2f7365f12e495
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-04-28 21:11:57 -0300

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 
 import classes from "./App.module.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className={classes.app}>
-      <h1>GitHub Finder</h1>
-      <Outlet />
+    <div>
+      <div className={classes.app}>
+        <h1>GitHub Finder</h1>
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 }
