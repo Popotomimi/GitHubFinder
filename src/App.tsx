01fab9907204fb5ec47453751a99756973bd22f1
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-04-28 21:21:29 -0300

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 
 import classes from "./App.module.css";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <div className={classes.app}>
         <h1>GitHub Finder</h1>
         <Outlet />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h3>React Finder</h3>
+    </nav>
+  );
+};
+
+export default Navbar;
